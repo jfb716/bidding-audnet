65a8f750677befc20af16043accf11e546a332f3
John Black <john.f.black@icloud.com>
2018-03-05 11:52:36 -0500

    only runs if PBJS object is present

--- bidding-extension/contentscript.js
+++ bidding-extension/contentscript.js
@@ -1,9 +1,7 @@
-if (pbjs) {
-  var s = document.createElement('script');
-  s.src = chrome.extension.getURL('script.js');
-  (document.head||document.documentElement).appendChild(s);
-  s.onload = function() {
-      s.parentNode.removeChild(s);
-  };
 
-}
+      var s = document.createElement('script');
+      s.src = chrome.extension.getURL('script.js');
+      (document.head||document.documentElement).appendChild(s);
+      s.onload = function() {
+          s.parentNode.removeChild(s);
+      };
--- bidding-extension/script.js
+++ bidding-extension/script.js
@@ -1,61 +1,66 @@
-var output = output || [];
-var myBidIds = myBidIds || [];
-function fireTool(){
-  console.clear();
-  console.log('%c<-- START -->', 'color: white; font-weight: bold;');
-  console.group(' -- Audience Network -- ');
+if (typeof pbjs != "undefined") {
+  console.log("JOHN: PREBID.JS DETECTED, STARTING TOOL");
+  var output = output || [];
+  var myBidIds = myBidIds || [];
+  function fireTool(){
+    console.clear();
+    console.log('%c<-- START -->', 'color: white; font-weight: bold;');
+    console.group(' -- Audience Network -- ');
 
 
 
 
-  var responses = pbjs.getBidResponses();
+    var responses = pbjs.getBidResponses();
 
-  for (var adunit in responses) {
-      if (responses.hasOwnProperty(adunit)) {
-          var bids = responses[adunit].bids;
-          for (var i = 0; i < bids.length; i++) {
-                 var b = bids[i];
-                  if (b.bidder === 'audienceNetwork' && myBidIds.indexOf(b.adId) === -1){
-                    anBidsCount += 1;
-                    output.push({
-                    'bidder': b.bidder, 'adunit': adunit, 'sizes': b.size, 'format': b.fb_format, 'placementId': b.fb_placementid, 'adId': b.adId, 'bidId': b.fb_bidid, 'bid': b.pbHg, 'status': b.statusMessage
-                    });
+    for (var adunit in responses) {
+        if (responses.hasOwnProperty(adunit)) {
+            var bids = responses[adunit].bids;
+            for (var i = 0; i < bids.length; i++) {
+                   var b = bids[i];
+                    if (b.bidder === 'audienceNetwork' && myBidIds.indexOf(b.adId) === -1){
+                      anBidsCount += 1;
+                      output.push({
+                      'bidder': b.bidder, 'adunit': adunit, 'sizes': b.size, 'format': b.fb_format, 'placementId': b.fb_placementid, 'adId': b.adId, 'bidId': b.fb_bidid, 'bid': b.pbHg, 'status': b.statusMessage
+                      });
 
-                    myBidIds.push(b.adId);
-                  }
-          }
-      }
-  }
+                      myBidIds.push(b.adId);
+                    }
+            }
+        }
+    }
 
-  if (typeof ADNW === 'object') {
-    var anWinsCount = ADNW.v55.ads.length;
-  } else {
-    var anWinsCount = 0;
-  }
-  var anBidsCount = output.length;
+    if (typeof ADNW === 'object') {
+      var anWinsCount = ADNW.v55.ads.length;
+    } else {
+      var anWinsCount = 0;
+    }
+    var anBidsCount = output.length;
 
-  console.log("Audience Network Bids: " + anBidsCount);
-  console.log("Audience Network Wins: " + anWinsCount);
+    console.log("Audience Network Bids: " + anBidsCount);
+    console.log("Audience Network Wins: " + anWinsCount);
 
-  if (output.length) {
-      if (console.table) {
-          console.table(output);
-      } else {
-          for (var j = 0; j < output.length; j++) {
-              console.log(output[j]);
-          }
-      }
-  } else {
-      console.warn('No Bids From Audience Network');
-  }
+    if (output.length) {
+        if (console.table) {
+            console.table(output);
+        } else {
+            for (var j = 0; j < output.length; j++) {
+                console.log(output[j]);
+            }
+        }
+    } else {
+        console.warn('No Bids From Audience Network');
+    }
 
-  console.groupEnd();
-  console.log('%c<-- FINISH ----->', 'color: white; font-weight: bold;');
-};
+    console.groupEnd();
+    console.log('%c<-- FINISH ----->', 'color: white; font-weight: bold;');
+  };
 
 
-var y = "<button class=\"toolButton\" onclick=\"fireTool()\">Print Bidding to Console</button>";
+  var y = "<button class=\"toolButton\" onclick=\"fireTool()\">Print Bidding to Console</button>";
 
-var z = "<style>.toolButton{height:50px\; width: 100px\;position: fixed; z-index:10000000000; top: 0; right:0; background-color: #3B5998; color: white; font-size: 12px; border-radius: 20px; padding: 5px; font-weight: bold; white-space:normal; text-shadow: 0 1px 3px rgba(0,0,0,.4); border: 2px solid white;}button:focus {outline:0;}</style>";
+  var z = "<style>.toolButton{height:50px\; width: 100px\;position: fixed; z-index:10000000000; top: 0; right:0; background-color: #3B5998; color: white; font-size: 12px; border-radius: 20px; padding: 5px; font-weight: bold; white-space:normal; text-shadow: 0 1px 3px rgba(0,0,0,.4); border: 2px solid white;}button:focus {outline:0;}</style>";
 
-document.body.innerHTML += y + z;
+  document.body.innerHTML += y + z;
+} else {
+  console.log("JOHN: NOT PREBID.JS");
+}
